d19ea861213cd2b729754911b0589f57ae320ba1
Jake Bixby <trabus@gmail.com>
2019-12-28 10:20:17 -0800

    more tests

--- tests/integration/components/modal-dialog-test.js
+++ tests/integration/components/modal-dialog-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { click, render } from '@ember/test-helpers';
+import { click, render, setupOnerror } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | modal-dialog', function(hooks) {
@@ -38,5 +38,71 @@ module('Integration | Component | modal-dialog', function(hooks) {
 
     await click('[data-test-close-button]');
     assert.dom(this.element).hasText('open');
-  })
+  });
+
+  test('it throws on a duplicate id', async function(assert) {
+    setupOnerror(function(err) {
+      assert.equal(err.message, "Assertion Failed: Could not register modal-dialog:test. The modal service already has a modal-dialog registered under the id: test. Check your templates for duplicate <ModalDialog @id=\"test\" ></ModalDialog> instances.");
+    });
+    await render(hbs`
+      <ModalDialog @id="test">template block text</ModalDialog>
+      <ModalDialog @id="test">template block text</ModalDialog>
+      <ModalTarget />
+    `);
+  });
+
+  test('it takes an onClose action', async function(assert) {
+    assert.expect(2);
+    this.set('hasClosed', false);
+    this.set('onClose', () => {
+      this.set('hasClosed', true);
+      assert.ok('onClose called');
+    })
+    await render(hbs`
+      <button type="button" data-test-open-button {{on "click" (fn this.modal.openModal "test")}}>open</button>
+      <ModalDialog data-test-modal-dialog @id="test" @onClose={{this.onClose}}>
+        template block text
+      </ModalDialog>
+      <ModalTarget />
+    `);
+    await click('[data-test-open-button]');
+    await click('[data-close-modal-dialog]');
+    assert.ok(this.hasClosed);
+  });
+
+  test('it takes an onOpen action', async function(assert) {
+    assert.expect(2);
+    this.set('hasOpened', false);
+    this.set('onOpen', () => {
+      this.set('hasOpened', true);
+      assert.ok('onOpen called');
+    })
+    await render(hbs`
+      <button type="button" data-test-open-button {{on "click" (fn this.modal.openModal "test")}}>open</button>
+      <ModalDialog data-test-modal-dialog @id="test" @onOpen={{this.onOpen}}>
+        template block text
+      </ModalDialog>
+      <ModalTarget />
+    `);
+    await click('[data-test-open-button]');
+    assert.ok(this.hasOpened);
+  });
+
+  test('shouldisBackdropClickable', async function(assert) {
+    this.set('isBackdropClickable', false);
+    await render(hbs`
+      <button type="button" data-test-open-button {{on "click" (fn this.modal.openModal "test")}}>open</button>
+      <ModalDialog data-test-modal-dialog @id="test" @isBackdropClickable={{this.isBackdropClickable}}>
+        <div data-test-content>template block text</div>
+      </ModalDialog>
+      <ModalTarget />
+    `);
+    await click('[data-test-open-button]');
+    await click('[data-modal-backdrop]');
+    assert.dom('[data-test-content]').isVisible();
+
+    this.set('isBackdropClickable', true);
+    await click('[data-modal-backdrop]');
+    assert.dom('[data-test-content]').isNotVisible();
+  });
 });
